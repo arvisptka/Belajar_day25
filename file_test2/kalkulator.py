0099177c94d69fa8e72ba1ac6590d762b83585c7
Arvisptka <arvisptkabunga@gmail.com>
2024-07-27 14:28:53 +0700

    penambahan file untuk fixing bug ceritanya

---
+++ file_test2/kalkulator.py
@@ -0,0 +1,10 @@
+def calculator():
+    nomor1 = float(input("masukan angka pertama : "))
+    nomor2 = float(input("masukan angka kedua : "))
+    return nomor1 + nomor2
+
+
+def calculator():
+    nomor1 = float(input("masukan angka pertama : "))
+    nomor2 = float(input("masukan angka kedua : "))
+    print("the sum of the two number is : ", nomor1+nomor2)
